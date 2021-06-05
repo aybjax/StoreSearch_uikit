b2271f28f2a2422c4ee161464d9ef3ecc653a0bc
aybjax <aybat93@gmail.com>
2021-06-06 05:11:00 +0600

    customization of appearance + before synchronous network

--- StoreSearch/AppDelegate.swift
+++ StoreSearch/AppDelegate.swift
@@ -14,6 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        customizeAppearance()
+        
         return true
     }
 
@@ -31,6 +34,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
+    func customizeAppearance() {
+        let barTintColor = UIColor(red: 20/255.0,
+                                   green: 160/255.0,
+                                   blue: 160/255.0,
+                                   alpha: 1)
+        UISearchBar.appearance().barTintColor = barTintColor
+    }
 }
 
--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -26,6 +26,7 @@ class SearchViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchBar.becomeFirstResponder()
         // Do any additional setup after loading the view.
         tableView.contentInset = UIEdgeInsets(top: 64, left: 0,
                                               bottom: 0, right: 0)
