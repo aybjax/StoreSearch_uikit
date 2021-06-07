bbe8f0d562362bdaf51a6676ee2fca03dffafeb2
aybjax <aybat93@gmail.com>
2021-06-08 02:17:58 +0600

    animations

--- StoreSearch/DetailViewController.swift
+++ StoreSearch/DetailViewController.swift
@@ -47,6 +47,8 @@ class DetailViewController: UIViewController {
         if searchResult != nil {
             updateUI()
         }
+        
+        view.backgroundColor = UIColor.clear
     }
     
     @IBAction func close() {
@@ -114,6 +116,14 @@ extension DetailViewController: UIViewControllerTransitioningDelegate {
                                 presenting: UIViewController?,
                                 source: UIViewController) -> UIPresentationController? {
         return DimmingPresentationController(presentedViewController: presented, presenting: presenting)
+    }
+    
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return BounceAnimationController()
+    }
+    
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return SlideOutAnimationController()
     }
 }
 
